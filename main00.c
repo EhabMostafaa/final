d5ea9fc2ead4dd16e6d944402b80ea73b0c483bb
ehab mostafa <ehab02166@gmail.com>
2021-04-24 17:13:25 +0200

    insert function

--- main00.c
+++ main00.c
@@ -3,6 +3,34 @@
 #include <string.h>
 #define NULL ( (void *) 0)
 
+void insert_node(struct node*element)
+{
+    struct node*temp;
+    temp=start;
+    if ((start==NULL)&&(end==NULL))
+    {
+        append_node(element);
+    }
+    else
+    {
+        printf("where you want to insert element \n 1) insert in beginning\n 2)insert after element\n 3)insert in end ");
+        scanf("%d",&choose);
+        switch(choose)
+        {
+        case'1': {temp->next=start;
+                start=temp;
+        }
+    case'2':{
+        }
+
+default :
+    {
+        append_node(element);
+    };
+};
+};
+}
+
 int choose;
 
 void print_list(void)
